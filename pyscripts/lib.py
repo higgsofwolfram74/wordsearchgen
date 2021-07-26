4c30e4a84ddcef99213f740a5dab4fd65241cba4
Mark Riley Bomberry <higgsofwolfram74@gmail.com>
2021-07-25 22:13:03 -0500

    Fleshing out the pieces

--- pyscripts/lib.py
+++ pyscripts/lib.py
@@ -1,11 +1,44 @@
+import random
 from dataclasses import dataclass, field
-from abc import ABC, abstractclassmethod
 from typing import List
 
 import numpy as np
 from nptyping import NDArray, Unicode
 
 
+SHORTEST_WORD_LEN = 2
+
+
+class Dictionary:
+
+    #open document with words to use
+    def __init__(self, path):
+        with open(path, 'r') as f:
+            self.lexicon = set([word for word in f.readlines()])
+
+    
+    #pick words to pad wordlist
+    def random_words(self, num_of_words=1000):
+        wordlist = []
+        for i in range(num_of_words):
+            wordlist.append(random.choic(self.lexicon))
+
+        return wordlist
+
+    
+    #check if word exists
+    def word_check(self, word: str):
+        return word in self.lexicon
+
+
+    def word_bubble(word: str, n: int):
+        if word.len() == SHORTEST_WORD_LEN:
+            return 6
+        elif word.len() > SHORTEST_WORD_LEN:
+            return 6 + (word.len() - 2) * 2
+        else:
+            print("Invalid word size...")
+            return 0
 
 class Wordsearch:
 
@@ -17,14 +50,29 @@ class Wordsearch:
 
     #see internal state
     def __str__(self):
-        print(self.wordsearch)
+        return f"{self.wordsearch}"
 
 
 @dataclass
 class Words:
     
     words: List[str]
+
+    #every word needs a letter buffer around it. Returns lowest possible size
+    def word_bubble(word: str, n: int):
+        if word.len() == SHORTEST_WORD_LEN:
+            return 6
+        elif word.len() > SHORTEST_WORD_LEN:
+            return 6 + (word.len() - 2) * 2
+        else:
+            print("Invalid word size...")
+            return 0
+
+
+    def by_len(word: str):
+        return word.len()
     
+
     def get_words(self):
         user = ''
         while user != 'n':
@@ -33,17 +81,26 @@ class Words:
             #smallest words I'll accept is two letter words
             if user.len() >= 2:
                 self.words.append(user)
+        
+        if self.words:
+            self.words.sort(reverse=True, key = Words.by_len())
+
 
     def load_words(self, path):
         with open(path, 'r') as f:
-            self.words = [word for word in f.readlines().strip()]
+            self.words = [word for word in f.readlines().strip()].sorted(reverse=True, key=Words.by_len())
+
+    
 
 
-@dataclass
+
 class WordsearchGenerator:
 
     wordsearch: Wordsearch
     wordlist: Words
+
+    def __init__(self, wordlist: Words):
+        self.wordlist = wordlist
 
 
     #pick random spots to put words, a random direction, and throw the word down
